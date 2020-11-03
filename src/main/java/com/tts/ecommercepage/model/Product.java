2b2103ae8962f3b42314d692253be93ca9cd5171
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-03 11:01:47 -0600

    initializing different getters and setters for products

---
+++ src/main/java/com/tts/ecommercepage/model/Product.java
@@ -0,0 +1,114 @@
+package com.tts.ecommercepage.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
+    private Long id;
+
+    private int quantity;
+
+    private double price;
+
+    private String description;
+
+    private String name;
+
+    private String brand;
+
+    private String category;
+
+    private String image;
+
+
+    //JPA needs empty constructor
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+//don't think we need a setter for id
+
+    // public void setId(Long id) {
+    //     this.id = id;
+    // }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [brand=" + brand + ", category=" + category + ", description=" + description + ", id=" + id
+                + ", image=" + image + ", name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+    }
+
+    
+
+    
+
+    
+
+}

e90d54e62b0051dad538e35e8ea2b87017df5de7
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-03 11:02:23 -0600

    user repo

---
+++ src/main/java/com/tts/ecommercepage/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.tts.ecommercepage.repositories;
+
+import com.tts.ecommercepage.model.User;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+    
+}

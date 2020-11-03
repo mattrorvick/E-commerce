1cdc68c1abb9c1b92752af4ba8bd5a85c85037a0
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-03 11:02:17 -0600

    product repo

---
+++ src/main/java/com/tts/ecommercepage/repositories/ProductRepository.java
@@ -0,0 +1,28 @@
+package com.tts.ecommercepage.repositories;
+
+import java.util.List;
+
+import com.tts.ecommercepage.model.Product;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    
+
+    List<Product> findAll();
+    Product findById(long id);
+    List<Product> findByBrand(String brand);
+    List<Product> findByCategory(String category);
+    List<Product> findByBrandAndCategory(String brand, String category);
+ 
+    @Query("SELECT DISTINCT p.brand FROM Product p")
+    List<String> findDistinctBrands();
+ 
+    @Query("SELECT DISTINCT p.category FROM Product p")
+    List<String> findDistinctCategories();
+
+
+}

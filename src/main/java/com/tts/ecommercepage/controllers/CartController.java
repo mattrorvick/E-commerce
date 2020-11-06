29cc63cb0b565196e36219eea149410732d2a727
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-06 07:34:34 -0600

    stripe public key for cc transaction

--- src/main/java/com/tts/ecommercepage/controllers/CartController.java
+++ src/main/java/com/tts/ecommercepage/controllers/CartController.java
@@ -10,7 +10,9 @@ import com.tts.ecommercepage.services.ProductService;
 import com.tts.ecommercepage.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +30,9 @@ public class CartController {
 
     @Autowired
     UserService userService;
+
+    @Value("${STRIPE_PUBLIC_KEY}")
+    private String stripePublicKey;
 
     @ModelAttribute("loggedInUser")
     public User loggedInUser() {
@@ -52,8 +57,10 @@ public class CartController {
       }
  
       @GetMapping("/cart")
-      public String showCart() {
-          return "cart";
+      public String showCart(Model model) {
+        model.addAttribute("amount", 100);
+        model.addAttribute("stripePublicKey", stripePublicKey);
+        return "cart";
       }
  
       @PostMapping("/cart")
